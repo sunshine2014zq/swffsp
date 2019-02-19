922eeab6787e459c5af0c3bd565ec065670073e9
sunshine2014zq <1475785135@qq.com>
2019-02-19 16:15:26 +0800

    创建项目

---
+++ src/main/java/com/sun/swffsp/SwffspApplication.java
@@ -0,0 +1,13 @@
+package com.sun.swffsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwffspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwffspApplication.class, args);
+    }
+
+}
