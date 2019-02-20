5772656bbb9f0e18ffaba13ac449fa1c5b711255
sunshine2014zq <1475785135@qq.com>
2019-02-20 14:16:13 +0800

    前端登录页面

---
+++ src/main/resources/static/js/base/baseUtils.js
@@ -0,0 +1,78 @@
+/**
+ * Created by sun on 2019/2/15 16:24
+ * use jquery-1.9.1
+ * use layer-2.4
+ */
+
+var baseUtils = {
+    //判断对象的value是否为空--item : 使用jquery选择器
+    isEmpty : function (item) {
+        if (item.val() == '' || item.val() == undefined) {
+            return true;
+        }
+        return false;
+    },
+    //1:成功 2:错误 3：疑问 4：锁定 5：哭脸，6：笑脸 7：警告
+    tip: function (msg, icon, time) {
+        layer.msg(msg, {icon: icon, time: time});
+    },
+
+    /**
+     * 包含加载遮罩层的post请求
+     * failCallBack 为空时提示“系统繁忙!请稍后重试”,否则执行回调failCallBack
+     */
+    post: function (vue, url, data, successCallBack, failCallBack) {
+        var loading = layer.load(0, {shade: [0.5, '#949494']}); //0代表加载的风格，支持0-2
+        vue.$http.post(url, data).then(function (response) {
+            layer.close(loading);
+            //回调
+            successCallBack(response);
+        }, function (response) {
+            console.log(response)
+            layer.close(loading);
+            if($.isFunction(failCallBack)){
+                //回调
+                failCallBack(response)
+            }else {
+                baseUtils.tip("系统繁忙!请稍后重试",2,1500);
+            }
+        });
+    },
+
+    /**
+     * 包含加载遮罩层的get请求
+     */
+    get: function (vue, url, successCallBack, failCallBack) {
+        var loading = layer.load(0, {shade: [0.5, '#949494']}, "#addPanel"); //0代表加载的风格，支持0-2
+        vue.get(url).then(function (response) {
+            //回调
+            successCallBack(response);
+            layer.close(loading);
+        }, function (response) {
+            console.log(response)
+            layer.close(loading);
+            if($.isFunction(failCallBack)){
+                //回调
+                failCallBack(response)
+            }else {
+                baseUtils.tip("系统繁忙!请稍后重试",2,1500);
+            }
+        });
+    }
+
+}
+
+
+/**
+ * jQuery库扩展--序列化为对象
+ * @returns {Object}
+ */
+jQuery.prototype.serializeObject=function(){
+    var obj=new Object();
+    $.each(this.serializeArray(),function(index,param){
+        if(!(param.name in obj)){
+            obj[param.name]=param.value;
+        }
+    });
+    return obj;
+}
---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,26 @@
+/**
+ * Created by sun on 2019/2/15 16:03
+ */
+
+//登录
+var loginUrl = "/swffsp/login";
+var indexUrl = "/swffsp/index.html";
+
+$(".loginBtn").click(function () {
+    //空验证-账户
+    if(baseUtils.isEmpty($("input[name='username']"))){
+        baseUtils.tip("账号不能为空",2,2000);
+        return;
+    }
+    var formData = $(".loginForm").serializeObject();
+    baseUtils.post(new Vue(),loginUrl,formData,function (response) {
+        console.log(response)
+
+        if(response.body.status){
+            window.location.href = indexUrl;
+            baseUtils.tip(response.body.msg,1,1500);
+        }else {
+            baseUtils.tip(response.body.msg,2,1500);
+        }
+    })
+});
