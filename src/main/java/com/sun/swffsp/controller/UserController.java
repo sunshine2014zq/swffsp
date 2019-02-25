6c10f258ea1183a71698d57ed01ad2e1a8d6686c
sunshine2014zq <1475785135@qq.com>
2019-02-25 16:35:19 +0800

    getUserInfo改名info

--- src/main/java/com/sun/swffsp/controller/UserController.java
+++ src/main/java/com/sun/swffsp/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.sun.swffsp.controller;
 
+import com.sun.swffsp.dto.db.UserEntity;
 import com.sun.swffsp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,10 +21,16 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/getUserInfo")
+    @RequestMapping("/info")
     @ResponseBody
     public Object getUserInfo() {
         return userService.getUserInfo();
     }
+
+    @RequestMapping("/list")
+    public Page<UserEntity> getUserList(){
+        return userService.getUserList();
+    }
+
 
 }
